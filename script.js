f102fff9d36ac38ddc6ba4b39957b215bc241e27
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-27 13:48:08 +0100

    created a button 'New Book' that open the side bar when clicked

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function Book(title,author,pages,read){
   this.pages = pages 
   this.read = read 
   this.info = function () {
-    return `${this.title} by ${this.author},${this.pages}, {this.read ? 'read' : 'not read yet}`
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+
   }
 }
 function addBookToLibrary (title,author,pages,read) {
@@ -14,5 +15,37 @@ function addBookToLibrary (title,author,pages,read) {
   myLibrary.push(theBook); 
   
 }
+
+function presentTheBooks(myLibrary) {
+for(let i=0 ; i<myLibrary.length ; i++){
+  console.log(myLibrary[i])
+}
+}
+
+/*
+function toggleSideBar () {
+document.getElementById("new-book").addEventListener('click' , function (){
+const sidebar=  document.getElementById("slide");
+if (sidebar.style.transform === 'translateX(-180px)') {
+sidebar.style.transform ='translateX(0)';
+}
+})
+
+}
+*/
+
+function toggleSideBar() {
+  const sidebar = document.getElementById("slide");
+  if (sidebar.style.transform === 'translateX(-180px)') {
+      sidebar.style.transform = 'translateX(0)';
+  } else {
+      sidebar.style.transform = 'translateX(-180px)';
+  }
+}
+
 console.log(addBookToLibrary("bob","doe",45,true))
-console.log(myLibrary)
+console.log(addBookToLibrary("bob","doe",45,true))
+console.log(addBookToLibrary("bob","doe",45,true))
+console.log(presentTheBooks(myLibrary))
+console.log(myLibrary);
+console.log(theBook.info())
