8c9aee1b2910f438d6c0d6bf97ae04600c0d55d9
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-03-29 23:18:58 +0100

    Book function refactored to a Book class

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    
+    info() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
-    };
+    }
+    
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
+
 
 function addBookToLibrary(title, author, pages, read) {
     const theBook = new Book(title, author, pages, read);
